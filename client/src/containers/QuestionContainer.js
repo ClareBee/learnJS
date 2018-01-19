886441f090058825797fe3e9f20fec4285f0b35b
Clare <chblackburne@gmail.com>
2018-01-19 10:22:48 +0000

    Removes duplicates from topic list

--- client/src/containers/QuestionContainer.js
+++ client/src/containers/QuestionContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavLink }from 'react-router-dom';
+import uniqBy from 'lodash/uniqBy';
 
 class QuestionContainer extends React.Component {
     constructor(props){
@@ -20,8 +21,9 @@ class QuestionContainer extends React.Component {
 
 
   render(){
-
-    const questions = this.props.questions.questions.map((question, index) => {
+    const originalData = this.props.questions.questions;
+    const duplicatesRemoved = uniqBy(originalData, 'topic');
+    const questions = duplicatesRemoved.map((question, index) => {
       let url = `/topics/${index}`;
       return  <li className="topic-item" onClick={this.handleClick} value={index}><NavLink to={url} key={index}>{question.topic}</NavLink></li>
     });
