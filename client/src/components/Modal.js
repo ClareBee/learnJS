7cace6eb4a5bb25f3d7ef2cd87f44a0b02d2f3cc
Clare <chblackburne@gmail.com>
2018-01-20 17:11:30 +0000

    Adds delete functionality to gif using current target of event

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,10 +3,19 @@ import React from 'react';
 class MyModal extends React.Component{
   constructor(props){
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e){
+    let gif = e.currentTarget;
+    gif.style.visibility = "hidden";
   }
   render(){
     return(
-      <div className="my-modal"><img src={this.props.image}/></div>
+      <div onClick={this.handleClick} className="my-modal">
+        <img src={this.props.image}/>
+        <div className="delete-gif">&times;</div>
+      </div>
     )
   }
 }
