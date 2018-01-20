f1fa90ab151228cc9c2b7bb755e99cd55e7e8328
Clare <chblackburne@gmail.com>
2018-01-20 12:05:19 +0000

    Adds drop box to container

--- client/src/containers/DragDropContainer.js
+++ client/src/containers/DragDropContainer.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import update from 'immutability-helper';
+import { DragDropContext } from 'react-dnd';
+import DragDropBox from '../components/DragDropBox';
+import HTML5Backend, { NativeTypes } from 'react-dnd-html5-backend';
 
-
+DragDropContext(HTML5Backend)
 class DragDropContainer extends React.Component {
   constructor(props){
     super(props);
   }
   render(){
     return(
+      <div>
       <h1>Drag and Drop Container</h1>
+      <DragDropBox />
+    </div>
     )
   }
 }
